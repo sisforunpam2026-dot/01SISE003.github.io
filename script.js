de8940367fbf08fd5e5c02752bec81d8fb8ae612
sisforunpam2026-dot <sisfor.unpam2026@gmail.com>
2026-04-06 00:58:27 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+let slides = document.querySelectorAll('.slide');
+let index = 0;
+
+function showSlide() {
+  slides.forEach((slide) => {
+    slide.classList.remove('active');
+  });
+
+  slides[index].classList.add('active');
+
+  index++;
+  if (index >= slides.length) {
+    index = 0;
+  }
+}
+
+setInterval(showSlide, 3000); // ganti setiap 3 detik
+// Contact
+document.querySelector("form").addEventListener("submit", function(e){
+  e.preventDefault();
+  alert("Pesan berhasil dikirim!");
+});
+
